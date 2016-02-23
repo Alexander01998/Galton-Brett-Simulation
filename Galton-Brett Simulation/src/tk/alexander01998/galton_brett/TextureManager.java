a5afe67953c412e3225120df538ec2e1517fefa9
Alexander01998 <contact.wurstimperium@gmail.com>
2016-02-23 14:08:20 +0100

    Add TextureManager

---
+++ Galton-Brett Simulation/src/tk/alexander01998/galton_brett/TextureManager.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright © 2016 | Alexander01998 | All rights reserved.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package tk.alexander01998.galton_brett;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class TextureManager
+{
+	public static BufferedImage HINTERGRUND = getTexture("hintergrund");
+	public static BufferedImage KUGEL = getTexture("kugel");
+	public static BufferedImage KEIL_DREIECK = getTexture("keil-dreieck");
+	
+	public static BufferedImage getTexture(String name)
+	{
+		try
+		{
+			return ImageIO.read(TextureManager.class.getClassLoader()
+				.getResourceAsStream("textures/" + name + ".png"));
+		}catch(IOException e)
+		{
+			// TODO: Fehlermeldungen
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
