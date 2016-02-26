ea3bc7a6fa68b0769610304c2c630edd6bfe0dd7
Alexander01998 <contact.wurstimperium@gmail.com>
2016-02-26 06:48:48 +0100

    Add entities

---
+++ Galton-Brett Simulation/src/tk/alexander01998/galton_brett/simulation/Entity.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright © 2016 | Alexander01998 | All rights reserved.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package tk.alexander01998.galton_brett.simulation;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+public abstract class Entity
+{	
+	private final BufferedImage texture;
+	
+	public Entity(BufferedImage texture)
+	{
+		this.texture = texture;
+	}
+	
+	public void render(Graphics g, int posX, int posY)
+	{
+		g.drawImage(texture, posX * 64, posY * 64, null);
+	}
+}
---
+++ Galton-Brett Simulation/src/tk/alexander01998/galton_brett/simulation/Marble.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2016 | Alexander01998 | All rights reserved.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package tk.alexander01998.galton_brett.simulation;
+
+import tk.alexander01998.galton_brett.gui.TextureManager;
+
+public class Marble extends Entity
+{
+	public Marble()
+	{
+		super(TextureManager.MARBLE);
+	}
+}
