6fa267830c80dd3cf5ca6e05479f4cd6c23f770d
Alexander01998 <contact.wurstimperium@gmail.com>
2016-03-08 14:01:49 +0100

    :bug: Fix rendering

--- Galton-Brett Simulation/src/tk/alexander01998/galton_brett/gui/SimulationPanel.java
+++ Galton-Brett Simulation/src/tk/alexander01998/galton_brett/gui/SimulationPanel.java
@@ -7,18 +7,16 @@
  */
 package tk.alexander01998.galton_brett.gui;
 
+import java.awt.Canvas;
 import java.awt.Graphics;
-
-import javax.swing.JPanel;
+import java.awt.image.BufferStrategy;
 
 import tk.alexander01998.galton_brett.GaltonBrett;
 import tk.alexander01998.galton_brett.simulation.Entity;
 import tk.alexander01998.galton_brett.simulation.Marble;
 
-public class SimulationPanel extends JPanel
+public class SimulationPanel extends Canvas
 {
-	private float partialTicks;
-	private Thread thread;
 	
 	/**
 	 * Create the panel.
@@ -28,56 +26,44 @@ public class SimulationPanel extends JPanel
 		
 	}
 	
-	@SuppressWarnings("deprecation")
-	@Override
-	public void paintComponent(Graphics g)
-	{
-		super.paintComponent(g);
-		
-		if(GaltonBrett.simulation == null)
-		{
-			g.drawString("Wird geladen...", 4, 16);
-			return;
-		}
-		
-		// background
-		for(int x = 0; x < getWidth(); x += 256)
-			for(int y = 0; y < getHeight(); y += 256)
-				g.drawImage(TextureManager.BACKGROUND, x, y, null);
-		
-		// marbles
-		for(Marble marble : GaltonBrett.simulation.marbles)
-			marble.render(g, partialTicks);
-		
-		// wedges & tubes
-		Entity[][] grid = GaltonBrett.simulation.grid;
-		for(int x = 0; x < grid.length; x++)
-			for(int y = 0; y < grid[x].length; y++)
-			{
-				Entity entity = grid[x][y];
-				if(entity != null)
-					entity.render(g, x, y);
-			}
-		
-		// border
-		g.drawRect(0, 0, grid.length * 64, grid[0].length * 64);
-		
-		// continue simulation
-		if(thread != null)
-			thread.resume();
-	}
-	
-	@SuppressWarnings("deprecation")
 	public void render(float partialTicks)
 	{
-		// set fields
-		this.partialTicks = partialTicks;
-		thread = Thread.currentThread();
+		BufferStrategy bs = getBufferStrategy();
+		if(bs == null)
+		{
+			createBufferStrategy(3);
+			return;
+		}
+		Graphics g = bs.getDrawGraphics();
 		
-		// schedule rendering
-		repaint();
+		if(GaltonBrett.simulation == null)
+			g.drawString("Wird geladen...", 4, 16);
+		else
+		{
+			// background
+			for(int x = 0; x < getWidth(); x += 256)
+				for(int y = 0; y < getHeight(); y += 256)
+					g.drawImage(TextureManager.BACKGROUND, x, y, null);
+			
+			// marbles
+			for(Marble marble : GaltonBrett.simulation.marbles)
+				marble.render(g, partialTicks);
+			
+			// wedges & tubes
+			Entity[][] grid = GaltonBrett.simulation.grid;
+			for(int x = 0; x < grid.length; x++)
+				for(int y = 0; y < grid[x].length; y++)
+				{
+					Entity entity = grid[x][y];
+					if(entity != null)
+						entity.render(g, x, y);
+				}
+			
+			// border
+			g.drawRect(0, 0, grid.length * 64, grid[0].length * 64);
+		}
 		
-		// wait for rendering
-		thread.suspend();
+		g.dispose();
+		bs.show();
 	}
 }
