cf1791dd6315e0fa329f48f8f89ec56356d87cf1
Alexander01998 <contact.wurstimperium@gmail.com>
2016-03-22 10:55:00 +0100

    Use GridBagLayout for SettingsPanel

--- Galton-Brett Simulation/src/tk/alexander01998/galton_brett/gui/SettingsPanel.java
+++ Galton-Brett Simulation/src/tk/alexander01998/galton_brett/gui/SettingsPanel.java
@@ -7,16 +7,19 @@
  */
 package tk.alexander01998.galton_brett.gui;
 
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
-import javax.swing.SpringLayout;
-import javax.swing.event.ChangeListener;
 import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import tk.alexander01998.galton_brett.GaltonBrett;
-import java.awt.Font;
 
 public class SettingsPanel extends JPanel
 {
@@ -25,56 +28,23 @@ public class SettingsPanel extends JPanel
 	 */
 	public SettingsPanel()
 	{
-		SpringLayout springLayout = new SpringLayout();
-		setLayout(springLayout);
-		
-		JLabel lblN = new JLabel("Reihen mit Keilen (n)");
-		springLayout.putConstraint(SpringLayout.NORTH, lblN, 13,
-			SpringLayout.NORTH, this);
-		springLayout.putConstraint(SpringLayout.WEST, lblN, 10,
-			SpringLayout.WEST, this);
-		add(lblN);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{156, 0, 0};
+		gridBagLayout.rowHeights = new int[]{26, 0, 26, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
 		
 		JLabel lblM = new JLabel("Anzahl der Kugeln");
-		springLayout.putConstraint(SpringLayout.NORTH, lblM, 10,
-			SpringLayout.SOUTH, lblN);
-		springLayout.putConstraint(SpringLayout.WEST, lblM, 10,
-			SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.EAST, lblM, -311,
-			SpringLayout.EAST, this);
-		add(lblM);
-		
-		JSpinner spinnerN = new JSpinner();
-		springLayout.putConstraint(SpringLayout.WEST, spinnerN, 175,
-			SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.EAST, spinnerN, -10,
-			SpringLayout.EAST, this);
-		springLayout.putConstraint(SpringLayout.EAST, lblN, -16,
-			SpringLayout.WEST, spinnerN);
-		spinnerN.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		springLayout.putConstraint(SpringLayout.NORTH, spinnerN, 10,
-			SpringLayout.NORTH, this);
-		spinnerN.addChangeListener(new ChangeListener()
-		{
-			@Override
-			public void stateChanged(ChangeEvent e)
-			{
-				GaltonBrett.simulation.setN((int)spinnerN.getValue());
-			}
-		});
-		lblN.setLabelFor(spinnerN);
-		spinnerN.setModel(new SpinnerNumberModel(new Integer(
-			GaltonBrett.simulation.getN()), new Integer(1), null,
-			new Integer(1)));
-		add(spinnerN);
+		GridBagConstraints gbc_lblM = new GridBagConstraints();
+		gbc_lblM.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblM.insets = new Insets(0, 0, 5, 5);
+		gbc_lblM.gridx = 0;
+		gbc_lblM.gridy = 0;
+		add(lblM, gbc_lblM);
 		
 		JSpinner spinnerM = new JSpinner();
-		springLayout.putConstraint(SpringLayout.NORTH, spinnerM, -3,
-			SpringLayout.NORTH, lblM);
-		springLayout.putConstraint(SpringLayout.WEST, spinnerM, 0,
-			SpringLayout.WEST, spinnerN);
-		springLayout.putConstraint(SpringLayout.EAST, spinnerM, -10,
-			SpringLayout.EAST, this);
+		lblM.setLabelFor(spinnerM);
 		spinnerM.addChangeListener(new ChangeListener()
 		{
 			@Override
@@ -83,17 +53,18 @@ public class SettingsPanel extends JPanel
 				GaltonBrett.simulation.setM((int)spinnerM.getValue());
 			}
 		});
-		lblM.setLabelFor(spinnerM);
 		spinnerM.setModel(new SpinnerNumberModel(new Integer(
 			GaltonBrett.simulation.getM()), new Integer(1), null,
 			new Integer(1)));
-		add(spinnerM);
+		GridBagConstraints gbc_spinnerM = new GridBagConstraints();
+		gbc_spinnerM.fill = GridBagConstraints.HORIZONTAL;
+		gbc_spinnerM.anchor = GridBagConstraints.NORTH;
+		gbc_spinnerM.insets = new Insets(0, 0, 5, 0);
+		gbc_spinnerM.gridx = 1;
+		gbc_spinnerM.gridy = 0;
+		add(spinnerM, gbc_spinnerM);
 		
 		JSpinner spinnerP = new JSpinner();
-		springLayout.putConstraint(SpringLayout.NORTH, spinnerP, 36,
-			SpringLayout.SOUTH, spinnerN);
-		springLayout.putConstraint(SpringLayout.EAST, spinnerP, -10,
-			SpringLayout.EAST, this);
 		spinnerP.addChangeListener(new ChangeListener()
 		{
 			@Override
@@ -102,18 +73,53 @@ public class SettingsPanel extends JPanel
 				GaltonBrett.simulation.setP((float)spinnerP.getValue());
 			}
 		});
+		
+		JSpinner spinnerN = new JSpinner();
+		spinnerN.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		spinnerN.addChangeListener(new ChangeListener()
+		{
+			@Override
+			public void stateChanged(ChangeEvent e)
+			{
+				GaltonBrett.simulation.setN((int)spinnerN.getValue());
+			}
+		});
+		
+		JLabel lblN = new JLabel("Reihen mit Keilen (n)");
+		lblN.setLabelFor(spinnerN);
+		GridBagConstraints gbc_lblN = new GridBagConstraints();
+		gbc_lblN.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblN.insets = new Insets(0, 0, 5, 5);
+		gbc_lblN.gridx = 0;
+		gbc_lblN.gridy = 1;
+		add(lblN, gbc_lblN);
+		spinnerN.setModel(new SpinnerNumberModel(new Integer(
+			GaltonBrett.simulation.getN()), new Integer(1), null,
+			new Integer(1)));
+		GridBagConstraints gbc_spinnerN = new GridBagConstraints();
+		gbc_spinnerN.fill = GridBagConstraints.HORIZONTAL;
+		gbc_spinnerN.anchor = GridBagConstraints.NORTH;
+		gbc_spinnerN.insets = new Insets(0, 0, 5, 0);
+		gbc_spinnerN.gridx = 1;
+		gbc_spinnerN.gridy = 1;
+		add(spinnerN, gbc_spinnerN);
+		
+		JLabel lblP = new JLabel("Wahrscheinlichkeit (p)");
+		lblP.setLabelFor(spinnerP);
+		GridBagConstraints gbc_lblP = new GridBagConstraints();
+		gbc_lblP.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblP.insets = new Insets(0, 0, 0, 5);
+		gbc_lblP.gridx = 0;
+		gbc_lblP.gridy = 2;
+		add(lblP, gbc_lblP);
 		spinnerP.setModel(new SpinnerNumberModel(new Float(0.5F),
 			new Float(0F), new Float(1F), new Float(0.05F)));
-		add(spinnerP);
-		
-		JLabel lblWahrscheinlichkeitp = new JLabel("Wahrscheinlichkeit (p)");
-		springLayout.putConstraint(SpringLayout.NORTH, lblWahrscheinlichkeitp,
-			12, SpringLayout.SOUTH, lblM);
-		springLayout.putConstraint(SpringLayout.WEST, spinnerP, 8,
-			SpringLayout.EAST, lblWahrscheinlichkeitp);
-		springLayout.putConstraint(SpringLayout.WEST, lblWahrscheinlichkeitp,
-			0, SpringLayout.WEST, lblN);
-		add(lblWahrscheinlichkeitp);
+		GridBagConstraints gbc_spinnerP = new GridBagConstraints();
+		gbc_spinnerP.fill = GridBagConstraints.HORIZONTAL;
+		gbc_spinnerP.anchor = GridBagConstraints.NORTH;
+		gbc_spinnerP.gridx = 1;
+		gbc_spinnerP.gridy = 2;
+		add(spinnerP, gbc_spinnerP);
 		
 	}
 }
