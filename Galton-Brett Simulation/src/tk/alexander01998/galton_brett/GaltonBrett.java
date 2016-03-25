14527e9333df56000d6ceaa142b9a70386fa04a9
Alexander01998 <contact.wurstimperium@gmail.com>
2016-03-25 16:01:18 +0100

    Add version

--- Galton-Brett Simulation/src/tk/alexander01998/galton_brett/GaltonBrett.java
+++ Galton-Brett Simulation/src/tk/alexander01998/galton_brett/GaltonBrett.java
@@ -14,6 +14,7 @@ import tk.alexander01998.galton_brett.simulation.Simulation;
 
 public class GaltonBrett
 {
+	public static final String VERSION = "1.0";
 	public static MainFrame frame;
 	public static Simulation simulation;
 	
--- Galton-Brett Simulation/src/tk/alexander01998/galton_brett/gui/MainFrame.java
+++ Galton-Brett Simulation/src/tk/alexander01998/galton_brett/gui/MainFrame.java
@@ -29,7 +29,7 @@ public class MainFrame extends JFrame
 	 */
 	public MainFrame()
 	{
-		setTitle("Galton-Brett Simulation");
+		setTitle("Galton-Brett Simulation v" + GaltonBrett.VERSION);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 800, 800);
 		JPanel contentPane = new JPanel();
